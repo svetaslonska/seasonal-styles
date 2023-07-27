9b79e23a4a27881d1e21b0463deb730c827faa97
Svitlana Slonska <svetaslonska@gmail.com>
2023-07-26 22:49:00 -0700

    :update

--- js/seasonal-style.js
+++ js/seasonal-style.js
@@ -15,6 +15,34 @@ $("document").ready(function(){
                 $("#wear").attr("src", "images/spring-wear.jpg");
                 $("#logo").attr("src", "images/spring.gif");
             break;
+
+            case "summer":
+                $("html").css("background-color", "#EBA52B");
+                $("#slogan").text("Hot and fun days ahead!");
+                $("#wear").attr("src", "images/summer-wear.jpg");
+                $("#logo").attr("src", "images/summer.gif");
+            break;
+
+            case "fall":
+                $("html").css("background-color", "#A81124");
+                $("#slogan").text("Follow The Flow Of The Autumn Wind!");
+                $("#wear").attr("src", "images/fall-wear.jpg");
+                $("#logo").attr("src", "images/fall.gif");
+            break;
+
+            case "winter":
+                $("html").css("background-color", "#005393");
+                $("#slogan").text("Nothing burns like the cold!");
+                $("#wear").attr("src", "images/winter-wear.jpg");
+                $("#logo").attr("src", "images/winter.gif");
+            break;
+
+            case "default":
+                $("html").css("background-color", "pink");
+                $("#slogan").text("Outfitter for All Seasons!");
+                //$("#wear").attr("src", "four-seasones.jpg");
+                //$("#logo").attr("src", "four-seasons.gif");
+            break;
         }
 
 
